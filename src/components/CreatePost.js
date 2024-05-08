5b188209f865dd50a4ac0176aa5d0a350ec67b91
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-08 23:14:12 +0900

    impl: post container

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./CreatePost.css";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>Post your note.</h1>
+        <div className="inputPost">
+          <div>title</div>
+          <input type="text" placeholder="write your awesome title here" />
+        </div>
+        <div className="inputPost">
+          <div>Post</div>
+          <textarea placeholder="write your fantastic post here"></textarea>
+        </div>
+        <button className="postButton">Submit</button>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
