a1a124e71724a22a64a71454da0a148c99796a63
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-09 21:21:01 +0900

    impl: delete post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
-import { database } from '../firebase';
+import { database, auth } from '../firebase';
+import "./Home.css";
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
@@ -13,24 +14,30 @@ const Home = () => {
     getPosts();
   }, []);
 
+  const handleDelete = async (id) => {
+    console.log("delete");
+    await deleteDoc(doc(database, "posts", id));
+    window.location.reload();
+  }
+
   return (
     <div className="homepage">
-      {postList.map((post) => {
-        return (
-          <div key={post.id} className="postContents" >
-            <div className="postHeader">
-              <h1>{post.title}</h1>
-            </div>
-            <div className="postTextContainer">
-              {post.postText}
-            </div>
-            <div className="nameAndDeleteButton">
-              <h3>{post.author.username}</h3>
-              <button>Delete</button>
-            </div>
+      {postList.map((post) => (
+        <div key={post.id} className="postContents">
+          <div className="postHeader">
+            <h1>{post.title}</h1>
           </div>
-        );
-      })}
+          <div className="postTextContainer">
+            {post.postText}
+          </div>
+          <div className="nameAndDeleteButton">
+            <h3>{post.author.username}</h3>
+            {post.author.id === auth.currentUser?.uid && (
+              <button onClick={() => handleDelete(post.id)}>Delete</button>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
