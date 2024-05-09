2842808fa5e1ee6aa5461392e1eaf30d232038d1
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-09 13:17:26 +0900

    impl: fetch data from firebase and show posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { database } from '../firebase';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(database, "posts"));
+      setPostList(data.docs.map(doc => ({...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className="homepage">
+      {postList.map((post) => {
+        return (
+          <div key={post.id} className="postContents" >
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">
+              {post.postText}
+            </div>
+            <div className="nameAndDeleteButton">
+              <h3>{post.author.username}</h3>
+              <button>Delete</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   )
 }
 
